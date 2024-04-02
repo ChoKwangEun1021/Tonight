a1cf037339cf1d6696da4f5e59ca5c636ad36ac8
poemfarer <poemfarer@gmail.com>
2024-04-02 17:18:14 +0900

    게시판 sample 리사이클러뷰 구성 임시(토스트) 리스너 추가

---
+++ app/src/main/java/com/weare2024/tonight/adapter/ListAdapter.kt
@@ -0,0 +1,40 @@
+package com.weare2024.tonight.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.weare2024.tonight.data.Images
+import com.weare2024.tonight.databinding.ActivityChatingBinding.bind
+import com.weare2024.tonight.databinding.ListItemBinding
+
+class ListAdapter(val context: Context, val imageList: List<Images>) : Adapter<ListAdapter.VH>() {
+
+    inner class VH(val binding: ListItemBinding) : ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val layoutInflater= LayoutInflater.from(context)
+        val binding= ListItemBinding.inflate(layoutInflater, parent, false)
+        return VH(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val images= imageList[position]
+
+        holder.binding.iv.setImageResource(images.imgId)
+
+        holder.binding.root.setOnClickListener {
+            Toast.makeText(context, "${position}번 BoardDetail Activity로 연결", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
--- app/src/main/java/com/weare2024/tonight/fragment/ListFragment.kt
+++ app/src/main/java/com/weare2024/tonight/fragment/ListFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.weare2024.tonight.R
+import com.weare2024.tonight.adapter.ListAdapter
+import com.weare2024.tonight.data.Images
 import com.weare2024.tonight.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    var items: ArrayList<Images> = ArrayList()
+
     private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
@@ -16,6 +23,40 @@ class ListFragment : Fragment() {
     ): View? {
         binding = FragmentListBinding.inflate(layoutInflater, container, false)
         return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+        items.add(Images(R.drawable.sample))
+
+        binding.recyclerView.adapter= ListAdapter(requireContext(), items)
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(context, "글 작성 Activity로 연결", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
