bea3ca0098d022bd1ddd48f01e2a0ef4e6be4116
dlawlgns1994 <dlawlgns1994@gmail.com>
2024-04-02 13:16:27 +0900

    체팅 구현 완

--- app/src/main/java/com/weare2024/tonight/activites/ChatingActivity.kt
+++ app/src/main/java/com/weare2024/tonight/activites/ChatingActivity.kt
@@ -12,6 +12,7 @@ import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.firestore
 import com.weare2024.tonight.G
 import com.weare2024.tonight.R
+import com.weare2024.tonight.adapter.ChatAdapter
 import com.weare2024.tonight.databinding.ActivityChatingBinding
 import com.weare2024.tonight.firebase.ChatData
 import java.text.SimpleDateFormat
@@ -21,32 +22,47 @@ import java.util.Locale
 class ChatingActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityChatingBinding
-    var room:String = "Room #1"
+    var room: String = "Room #1"
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityChatingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val msgItem:MutableList<ChatData> = mutableListOf()
+        val msgItem: MutableList<ChatData> = mutableListOf()
 
         binding.toolvar.title = room
-        binding.toolvar.subtitle = "임지훈" //G.name
+        binding.toolvar.subtitle = G.name
         binding.btnSend.setOnClickListener { btnSend() }
+        binding.recyclerView.adapter = ChatAdapter(this,msgItem)
+        val chatRef: CollectionReference = Firebase.firestore.collection(room)
+        chatRef.addSnapshotListener { value, error ->
+            value?.documentChanges?.forEach {
+                val snapshot = it.document
+                val item = snapshot.toObject(ChatData::class.java)
+                item?.apply {
+                    msgItem.add(this)
+                    binding.recyclerView.adapter!!.notifyItemInserted(msgItem.size-1)
+                    binding.recyclerView.scrollToPosition(msgItem.size - 1)
+                }
+            }
+        }
 
     }
-    private fun btnSend(){
+
+    private fun btnSend() {
         val nickname = G.name
         val image = G.image
         val msg = binding.et.text.toString()
-        val time =SimpleDateFormat("HH:mm", Locale.KOREA).format(Date())
+        val time = SimpleDateFormat("HH:mm", Locale.KOREA).format(Date())
 
-        val item =ChatData(nickname,msg,time,image)
-        val chatRef:CollectionReference = Firebase.firestore.collection(room)
-        val t="Msg_"+SimpleDateFormat("yyyyMMddHHmmss", Locale.KOREA)
+        val item = ChatData(nickname, msg, time, image)
+        val chatRef: CollectionReference = Firebase.firestore.collection(room)
+        val t = "MSG_" + (SimpleDateFormat("yyyyMMddHHmmss", Locale.KOREA).format(Date()))
         chatRef.document(t).set(item)
         binding.et.setText("")
-        val inputMethodManager =getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken,0)
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
 
     }
--- app/src/main/java/com/weare2024/tonight/adapter/ChatAdapter.kt
+++ app/src/main/java/com/weare2024/tonight/adapter/ChatAdapter.kt
@@ -5,20 +5,28 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.weare2024.tonight.G
 import com.weare2024.tonight.databinding.RecyclerViewMyChatBinding
 import com.weare2024.tonight.databinding.RecyclerViewOtherChatBinding
 import com.weare2024.tonight.firebase.ChatData
 
-class ChatAdapter(var context: Context,var chatDataItem:List<ChatData> ): Adapter<ViewHolder>() {
+class ChatAdapter(var context: Context, var chatDataItem: List<ChatData>) : Adapter<ViewHolder>() {
 
-    inner class VH1(val binding:RecyclerViewMyChatBinding) :ViewHolder(binding.root)
-    inner class VH2(val binding:RecyclerViewOtherChatBinding) :ViewHolder(binding.root)
+    inner class VH1(val binding: RecyclerViewMyChatBinding) : ViewHolder(binding.root)
+    inner class VH2(val binding: RecyclerViewOtherChatBinding) : ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val laoutInflater =LayoutInflater.from(context)
+        val laoutInflater = LayoutInflater.from(context)
 
-        return if (viewType == 0) VH1(RecyclerViewMyChatBinding.inflate(laoutInflater,parent,false))
-        else VH2(RecyclerViewOtherChatBinding.inflate(laoutInflater,parent,false))
+        return if (viewType == 0) VH1(
+            RecyclerViewMyChatBinding.inflate(
+                laoutInflater,
+                parent,
+                false
+            )
+        )
+        else VH2(RecyclerViewOtherChatBinding.inflate(laoutInflater, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -27,6 +35,21 @@ class ChatAdapter(var context: Context,var chatDataItem:List<ChatData> ): Adapte
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = chatDataItem[position]
-        //aa
+        if (item.nickname == G.name) {
+            val vh = holder as VH1
+            vh.binding.tvName.text = item.nickname
+            vh.binding.tvMsg.text = item.message
+            vh.binding.tvTime.text = item.time
+//            Glide.with(context).load(item.profileUrl).into(vh.binding.ciriv)
+
+        } else {
+            val vh = holder as VH2
+            vh.binding.tvName.text = item.nickname
+            vh.binding.tvMsg.text = item.message
+            vh.binding.tvTime.text = item.time
+//            Glide.with(context).load(item.profileUrl).into(vh.binding.ciriv)
+
+        }
+
     }
 }
