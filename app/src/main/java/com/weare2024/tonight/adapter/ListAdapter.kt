0407b8c4e0f1a6fbb68fd97efd51c6e4dad0deda
ajksl2356 <ajksl2356@gmail.com>
2024-04-03 15:34:15 +0900

    이거아님

--- app/src/main/java/com/weare2024/tonight/adapter/ListAdapter.kt
+++ app/src/main/java/com/weare2024/tonight/adapter/ListAdapter.kt
@@ -1,9 +1,9 @@
 package com.weare2024.tonight.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.weare2024.tonight.data.Images
@@ -18,20 +18,20 @@ class ListAdapter(val context: Context, val imageList: List<Images>) : Adapter<L
         val binding= ListItemBinding.inflate(layoutInflater, parent, false)
         return VH(binding)
     }
-
     override fun getItemCount(): Int {
         return imageList.size
     }
-
     override fun onBindViewHolder(holder: VH, position: Int) {
         val images= imageList[position]
 
         holder.binding.iv.setImageResource(images.imgId)
 
         holder.binding.root.setOnClickListener {
-            Toast.makeText(context, "${position}번 BoardDetail Activity로 연결", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(context, "${position}번 BoardDetail Activity로 연결", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(context,BoardDetailActivity::class.java)
+//            context.startActivity(intent)
+            val intent = Intent(context,BoardDetailActivity::class.java)
+            context.startActivity(intent)
         }
-
     }
-
 }
