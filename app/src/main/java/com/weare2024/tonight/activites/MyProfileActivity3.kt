40ffbb552a54685b6aab1b7fd419750dd64665ce
nameskdlzm <nameskdlzm@gmail.com>
2024-04-03 17:51:25 +0900

    오류 수정

--- app/src/main/java/com/weare2024/tonight/activites/MyProfileActivity3.kt
+++ app/src/main/java/com/weare2024/tonight/activites/MyProfileActivity3.kt
@@ -2,15 +2,17 @@ package com.weare2024.tonight.activites
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.weare2024.tonight.databinding.ActivityMyProfile1Binding
+import com.weare2024.tonight.databinding.ActivityMyProfile3Binding
 
 class MyProfileActivity3 : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMyProfile1Binding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityMyProfile3Binding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
 
     }
 }
