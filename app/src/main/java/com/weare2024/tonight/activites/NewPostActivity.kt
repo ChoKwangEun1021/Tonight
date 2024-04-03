70e81f4bfe050810003b305222dbac7aae03b56e
poemfarer <poemfarer@gmail.com>
2024-04-03 17:54:30 +0900

    List Fragment 수정 게시글 작성 페이지 xml 완료

---
+++ app/src/main/java/com/weare2024/tonight/activites/NewPostActivity.kt
@@ -0,0 +1,19 @@
+package com.weare2024.tonight.activites
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.weare2024.tonight.R
+import com.weare2024.tonight.databinding.ActivityNewPostBinding
+
+class NewPostActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityNewPostBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
