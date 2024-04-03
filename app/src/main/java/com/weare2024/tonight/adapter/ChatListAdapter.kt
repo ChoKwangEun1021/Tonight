d58daa1babd15150b6d8445c7230371a9b758431
dlawlgns1994 <dlawlgns1994@gmail.com>
2024-04-03 17:50:18 +0900

    채팅 건드는중 / 회원가입 bg 완료 챗샘플리스트 완성

--- app/src/main/java/com/weare2024/tonight/adapter/ChatListAdapter.kt
+++ app/src/main/java/com/weare2024/tonight/adapter/ChatListAdapter.kt
@@ -1,4 +1,42 @@
 package com.weare2024.tonight.adapter
 
-class ChatListAdapter {
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.weare2024.tonight.activites.ChatingActivity
+import com.weare2024.tonight.data.SampleChatList
+
+import com.weare2024.tonight.databinding.FragmentChatBinding
+import com.weare2024.tonight.databinding.RecyclerViewChatListBinding
+
+
+class ChatListAdapter(var context: Context,var chatlist:List<SampleChatList>) :Adapter<ChatListAdapter.VH>(){
+    inner class VH(var binding: RecyclerViewChatListBinding):ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val layoutInflater = LayoutInflater.from(context)
+        var binding=RecyclerViewChatListBinding.inflate(layoutInflater,parent,false)
+        return VH(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return chatlist.size
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val list = chatlist[position]
+        holder.binding.tvLastText.text = list.aa
+        holder.binding.tvNickName.text = list.ss
+
+        holder.binding.root.setOnClickListener{
+            val intent = Intent(context,ChatingActivity::class.java)
+
+
+            context.startActivity(intent)
+        }
+    }
+
 }
--- app/src/main/java/com/weare2024/tonight/fragment/ChatFragment.kt
+++ app/src/main/java/com/weare2024/tonight/fragment/ChatFragment.kt
@@ -7,23 +7,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.weare2024.tonight.activites.ChatingActivity
+import com.weare2024.tonight.adapter.ChatListAdapter
+import com.weare2024.tonight.data.SampleChatList
 import com.weare2024.tonight.databinding.ActivityChatingBinding
 import com.weare2024.tonight.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
     private lateinit var binding: FragmentChatBinding
+    var sampleChatList:MutableList<SampleChatList> = mutableListOf()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         binding = FragmentChatBinding.inflate(layoutInflater, container, false)
+
+        sampleChatList.add(0,SampleChatList("옥지얌","빵빵이"))
+        sampleChatList.add(1,SampleChatList("ㅂㅈㄷ","ㅁㄴㅇ"))
+        sampleChatList.add(2,SampleChatList("123","345"))
+        sampleChatList.add(3,SampleChatList("asd","fga"))
+        sampleChatList.add(4,SampleChatList("바보얌","빵빵이"))
+        sampleChatList.add(5,SampleChatList("123123123","111"))
+        sampleChatList.add(6,SampleChatList("ㅁㄴㅇㅁㄴㅇ","빵빵ㄴㄴ이"))
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.recyclerView.adapter = ChatListAdapter(requireContext(),sampleChatList)
         binding.btn.setOnClickListener { clickBtn() }
         binding.btn2.setOnClickListener { clickBtn2() }
+
 
     }
 
