b659755652a800c162e4b8d5eb2d7edf18a198af
ajksl2356 <ajksl2356@gmail.com>
2024-04-04 17:52:08 +0900

    툴바 신고 구현 완료

--- app/src/main/java/com/weare2024/tonight/activites/BoardDetailActivity.kt
+++ app/src/main/java/com/weare2024/tonight/activites/BoardDetailActivity.kt
@@ -17,30 +17,23 @@ class BoardDetailActivity : AppCompatActivity() {
     private val bs: View by lazy { binding.bs } //댓글 바텀시트
 //    private val reportBs: View by lazy { R.layout.more112 } // 신고 바텀시트
     private val bsb: BottomSheetBehavior<View> by lazy { BottomSheetBehavior.from(bs) }
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         binding.request.setOnClickListener { clickComment() }
         binding.chat.setOnClickListener { clickChat() }
         binding.rl.background = null
-
         binding.toolbar.setOnMenuItemClickListener(object : OnMenuItemClickListener{
             override fun onMenuItemClick(item: MenuItem?): Boolean {
                 when(item!!.itemId){
                     R.id.more112->{
-
+                        showBottomSheet()
                     }
                 }
-
                 return true
             }
-
         })
-
     }
-
     private fun showBottomSheet(){
         val dailog = BottomSheetDialog(this@BoardDetailActivity)
         val view = layoutInflater.inflate(R.layout.more112,null)
