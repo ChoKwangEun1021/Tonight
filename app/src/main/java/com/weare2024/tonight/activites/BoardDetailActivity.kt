0cf19db606effee35372d307897b0f1c7300bedc
ajksl2356 <ajksl2356@gmail.com>
2024-04-04 16:33:32 +0900

    툴바 신고 구현 완료

--- app/src/main/java/com/weare2024/tonight/activites/BoardDetailActivity.kt
+++ app/src/main/java/com/weare2024/tonight/activites/BoardDetailActivity.kt
@@ -1,29 +1,53 @@
 package com.weare2024.tonight.activites
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.Toolbar.OnMenuItemClickListener
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.weare2024.tonight.R
 import com.weare2024.tonight.databinding.ActivityBoardDetailBinding
 
 class BoardDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityBoardDetailBinding.inflate(layoutInflater) }
-    private val bs: View by lazy { binding.bs }
+    private val bs: View by lazy { binding.bs } //댓글 바텀시트
+//    private val reportBs: View by lazy { R.layout.more112 } // 신고 바텀시트
     private val bsb: BottomSheetBehavior<View> by lazy { BottomSheetBehavior.from(bs) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.request.setOnClickListener { clickRequest() }
+
+        binding.request.setOnClickListener { clickComment() }
         binding.chat.setOnClickListener { clickChat() }
         binding.rl.background = null
+
+        binding.toolbar.setOnMenuItemClickListener(object : OnMenuItemClickListener{
+            override fun onMenuItemClick(item: MenuItem?): Boolean {
+                when(item!!.itemId){
+                    R.id.more112->{
+
+                    }
+                }
+
+                return true
+            }
+
+        })
+
     }
-    private fun clickRequest(){
+
+    private fun showBottomSheet(){
+        val dailog = BottomSheetDialog(this@BoardDetailActivity)
+        val view = layoutInflater.inflate(R.layout.more112,null)
+        dailog.setContentView(view)
+        dailog.show()
+    }
+    private fun clickComment(){
         if (bsb.state == BottomSheetBehavior.STATE_COLLAPSED)  // 상태 확인
             bsb.state = BottomSheetBehavior.STATE_EXPANDED // 시트 열기
         else {bsb.state = BottomSheetBehavior.STATE_COLLAPSED}  // 시트 닫기
