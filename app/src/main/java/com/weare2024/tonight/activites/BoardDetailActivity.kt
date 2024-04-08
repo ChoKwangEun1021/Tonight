9ae1d20ccb65712bf7f21dcf71b360adec898b34
ajksl2356 <ajksl2356@gmail.com>
2024-04-08 10:40:39 +0900

    툴바 신고 텍스트 선택 시 접수 완료 토스트 띄우기 성공!!!!!!!! 앱터지던거 해결했다 퓨ㅠㅠㅠ

--- app/src/main/java/com/weare2024/tonight/activites/BoardDetailActivity.kt
+++ app/src/main/java/com/weare2024/tonight/activites/BoardDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.weare2024.tonight.activites
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
@@ -17,7 +16,7 @@ import com.weare2024.tonight.databinding.ActivityBoardDetailBinding
 class BoardDetailActivity : AppCompatActivity() {
     private val binding by lazy { ActivityBoardDetailBinding.inflate(layoutInflater) }
     private val bs: View by lazy { binding.bs } //댓글 바텀시트
-//    private val reportBs: View by lazy { R.layout.more112 } // 신고 바텀시트
+//    private val more112 : View by lazy { R.id.more112 } // 신고 바텀시트
     private val bsb: BottomSheetBehavior<View> by lazy { BottomSheetBehavior.from(bs) }
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -26,37 +25,11 @@ class BoardDetailActivity : AppCompatActivity() {
         binding.chat.setOnClickListener { clickChat() }
         binding.rl.background = null
         binding.toolbar.setOnMenuItemClickListener(object : OnMenuItemClickListener{
-            @SuppressLint("SuspiciousIndentation")
             override fun onMenuItemClick(item: MenuItem?): Boolean {
                 if (item!!.itemId == R.id.more112) {
                     showBottomSheet()
-                    val singo1 = findViewById<TextView>(R.id.singo_1)
-                        singo1?.setOnClickListener {
-                            Toast.makeText(this@BoardDetailActivity, "${singo1.text}", Toast.LENGTH_SHORT).show()
 
                     }
-//                    val singo2:TextView = findViewById(R.id.singo_2)
-//                    singo2.setOnClickListener {
-//
-//                    }
-//                    val singo3:TextView = findViewById(R.id.singo_3)
-//                    singo3.setOnClickListener {
-//
-//                    }
-//                    val singo4:TextView = findViewById(R.id.singo_4)
-//                    singo4.setOnClickListener {
-//
-//                    }
-//                    val singo5:TextView = findViewById(R.id.singo_5)
-//                    singo5.setOnClickListener {
-//
-//                    }
-                }
-//                when(item!!.itemId){
-//                    R.id.more112->{
-//                        showBottomSheet()
-//                    }
-//                }
                 return true
             }
         })
@@ -66,6 +39,26 @@ class BoardDetailActivity : AppCompatActivity() {
         val dailog = BottomSheetDialog(this@BoardDetailActivity)
         val view = layoutInflater.inflate(R.layout.more112,null)
         dailog.setContentView(view)
+        val singo1 = view.findViewById<TextView>(R.id.singo_1)
+        singo1.setOnClickListener {
+            Toast.makeText(this@BoardDetailActivity, "신고가 접수 되었습니다", Toast.LENGTH_SHORT).show()
+        }
+        val singo2 = view.findViewById<TextView>(R.id.singo_2)
+        singo2.setOnClickListener {
+            Toast.makeText(this@BoardDetailActivity, "신고가 접수 되었습니다", Toast.LENGTH_SHORT).show()
+        }
+        val singo3 = view.findViewById<TextView>(R.id.singo_3)
+        singo3.setOnClickListener {
+            Toast.makeText(this@BoardDetailActivity, "신고가 접수 되었습니다", Toast.LENGTH_SHORT).show()
+        }
+        val singo4 = view.findViewById<TextView>(R.id.singo_4)
+        singo4.setOnClickListener {
+            Toast.makeText(this@BoardDetailActivity, "신고가 접수 되었습니다", Toast.LENGTH_SHORT).show()
+        }
+        val singo5 = view.findViewById<TextView>(R.id.singo_5)
+        singo5.setOnClickListener {
+            Toast.makeText(this@BoardDetailActivity, "신고가 접수 되었습니다", Toast.LENGTH_SHORT).show()
+        }
         dailog.show()
     }
     private fun clickComment(){
@@ -76,4 +69,5 @@ class BoardDetailActivity : AppCompatActivity() {
     private fun clickChat(){
         Toast.makeText(this, "채팅 채널로 연결 됩니다.", Toast.LENGTH_SHORT).show()
     }
+
 }
