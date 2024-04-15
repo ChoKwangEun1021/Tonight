5b035b7417b2ae1ab76f3604e7fdcdbe925ed9d7
ajksl2356 <ajksl2356@gmail.com>
2024-04-15 17:53:13 +0900

    씨부레 닉변 이미지 변경 성공 인데 ... 수정 해야함 닉변 빈칸일 때 null저장 된다 하니까 조심히 해야함

--- app/src/main/java/com/weare2024/tonight/activites/ChangeProfileActivity.kt
+++ app/src/main/java/com/weare2024/tonight/activites/ChangeProfileActivity.kt
@@ -1,12 +1,15 @@
 package com.weare2024.tonight.activites
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.VectorDrawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,10 +17,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.toObject
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.weare2024.tonight.G
 import com.weare2024.tonight.R
+import com.weare2024.tonight.data.UserData
 import com.weare2024.tonight.databinding.ActivityChangeProfileBinding
 import com.weare2024.tonight.firebase.FBAuth
 import com.weare2024.tonight.firebase.FBRef
@@ -27,6 +35,7 @@ class ChangeProfileActivity : AppCompatActivity() {
     private var imgUri: Uri? = null
     private val spf2 by lazy { getSharedPreferences("userInfo", MODE_PRIVATE) }
     private val spf2Edt by lazy { spf2.edit() }
+    val uid = G.uid
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -34,43 +43,60 @@ class ChangeProfileActivity : AppCompatActivity() {
         binding.btn.setOnClickListener { clickBtn() }
         binding.iv.setOnClickListener { getImage() }
 
+
+        FBRef.userRef.whereEqualTo("uid", uid).get().addOnSuccessListener {
+            for ( snap in it ){
+                val userData : UserData? = snap.toObject(UserData::class.java)
+                userData?.apply {
+                    val uri = uid
+                    val imgRef = Firebase.storage.getReference("usersImg/$uri")
+                    imgRef.downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri>{
+                        override fun onSuccess(p0: Uri?) {
+                            Glide.with(this@ChangeProfileActivity).load(p0).into(binding.iv)
+                        }
+                    })
+
+                }
+            }
+        }
     }
-    private fun clickRegister() {
-        val uid = G.uid
+    private fun clickBtn() {
+
 
         if (uid.isNotEmpty()) {
             // 사용자의 UID로 해당 사용자를 식별하여 Firestore에서 사용자 문서 가져오기
             FBRef.userRef.whereEqualTo("uid", uid).get().addOnSuccessListener { querySnapshot ->
                 for (document in querySnapshot.documents) {
+
                     val newNickname = binding.inputLayoutNickName.editText!!.text.toString()
                     // 사용자의 닉네임 업데이트
                     val newData = mutableMapOf<String, Any>() // 이거 한 이유 빈 거 만들어서 데이터 담을것
                     newData["nickname"] = newNickname
                     // 사용자 문서 업데이트
                     document.reference.set(newData, SetOptions.merge()).addOnSuccessListener {
-                        Toast.makeText(this, "닉네임이 성공적으로 업데이트되었습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "수정완료", Toast.LENGTH_SHORT).show()
                     }.addOnFailureListener { e ->
-                        Toast.makeText(this, "닉네임 업데이트에 실패했습니다: ${e.message}", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, ": ${e.message}", Toast.LENGTH_SHORT).show()
                     }
                 }
             }.addOnFailureListener { e ->
                 Toast.makeText(this, "사용자 문서를 가져오는 데 실패했습니다: ${e.message}", Toast.LENGTH_SHORT).show()
             }
         }
+        if (uid.isNotEmpty()){
+            // 사용자 UID로 해당 사용자를 식별
+            val uri = uid
+            val imgRef = Firebase.storage.getReference("usersImg/$uri")
+            imgUri?.apply {
+                imgRef.putFile(this).addOnSuccessListener {
+
+                }
+            }
+            finish()
+        }
     }
 
-    private fun clickBtn() {
-        if (binding.iv.drawable is VectorDrawable) {
-            Toast.makeText(this, "사진을 선택해 주세요", Toast.LENGTH_SHORT).show()
-            return
-        } else if (binding.inputLayoutNickName.editText!!.text.toString().isEmpty()) {
-            Toast.makeText(this, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
-            return
-        } else {
-            Toast.makeText(this, "사진을 선택해 주세요", Toast.LENGTH_SHORT).show()
-        }
-        clickRegister()
-    }
+
 
     private fun getImage() {
         val intent =
